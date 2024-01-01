d611025148100e09ecb8517f7dea23aa2f315829
Zhixu Zhao <zhixu001@126.com>
2024-01-01 09:24:20 +0800

    Set exec path from shell.

--- custom/setup-common.el
+++ custom/setup-common.el
@@ -14,6 +14,22 @@
       tool-bar-mode nil
       warning-suppress-types '((emacs)))
 
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match
+that used by the user's shell.
+
+This is particularly useful under Mac OS X and macOS, where GUI
+apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string
+			              "[ \t\n]*$" "" (shell-command-to-string
+					                      "$SHELL --login -i -c 'echo $PATH'"
+						                  ))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(set-exec-path-from-shell-PATH)
+
 (define-key key-translation-map (kbd "<menu>") #'event-apply-super-modifier) ; super key
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (windmove-default-keybindings)
