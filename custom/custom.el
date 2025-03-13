1a7339e1f6fd14de1b39a26a9794e7bf100c490f
Zhixu Zhao <zhixu001@foxmail.com>
2025-03-13 16:19:47 +0800

    Setup fonts on mac.

--- custom/custom.el
+++ custom/custom.el
@@ -85,7 +85,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Noto Mono" :foundry "GOOG" :slant normal :weight regular :height 151 :width normal))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "black" :foreground "gray80" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Monaco"))))
  '(header-line ((t (:inherit mode-line :background "grey20" :foreground "grey90" :inverse-video nil :underline t))))
  '(message-view-patch-commit-comment ((t nil)))
  '(message-view-patch-commit-message ((t nil)))
